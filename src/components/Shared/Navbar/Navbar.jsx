c02362ae319e99f8d104329df42dd69bc30cdcee
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-07-05 20:10:27 +0600

    add:navbar active links design for mobile

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -31,17 +31,10 @@ const Navbar = () => {
                         </svg>
                     </div>
                     <ul
+                    id="navbarLinksMobile"
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
                 <div className="flex gap-1">
